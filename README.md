15cce40cdfe55a1d3f680afc4137c54c1bb65e3c
junioalbino <junioalbino.si@gmail.com>
2020-04-05 12:06:44 -0300

    Implementando lógica para vários usuários utilizarem o elevador

---
+++ README.md
@@ -0,0 +1,23 @@
+# Sistema de elevador
+
+## Instruções para uso:
+
+* Instalar o NodeJs
+* Para clonar o projeto use o comando: `git clone https://github.com/junioalbino/elevador.git`
+* Para instalar as dependências do projeto usar o comando: `npm install`
+* Para compilar use o comando: `tsc index.ts`
+* Para rodar o programa: `node index.js`
+* Para observar mudanças no arquivo rode o comando: `nodemon`
+* Para compilar, executar e observar: rode o comando: `npm start`
+
+## Requisitos funcionais
+
+* O elevador deve oferecer uma interface para que o usuário escolha um andar
+* O sistema deverá validar o andar escolhido
+    * O número deverá estar entre 0 e 10
+* O elevador deve exibir o andar escolhido
+* O sistema deverá permitir que mais de um usuário escolha seu andar
+
+## Requisitos não funcionais
+
+* O sistema não deverá permitir que o usuário digite uma string
--- index.ts
+++ index.ts
@@ -1,18 +1,40 @@
 // importar um módulo para ler o que o usuário digitou
-const readline = require('readline');
+import { createInterface } from "readline";
 
 // cria a interface para usar a entrada e saída do terminal
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const rl = createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false
 });
 
-// faz a pergunta para o usuáio (input, entrada)
-rl.question('Elevador: Digite o andar desejado: ', (answer) => {
+const andarInicial : Number = 0;
+const andarFinal : Number = 20;
 
-    // exibe o que o usuário digitou (output, saída)
-    console.log(`Andar escolhido: ${answer}`);
+function escolha() {
+    // faz a pergunta para o usuáio (input, entrada)
+    rl.question('Elevador: Digite o andar desejado: ', (andarStr : string) => {
 
-    // fecha a aplicação
-    rl.close();
-});
+        const andarNum = Number(andarStr);
+        
+        // Se o andarNum for NaN, o sistema deverá ignorar
+        if (isNaN(andarNum)) {
+            escolha();
+        } else {
+            // Se o número digitado estiver fora do limite
+            // deverá ser exibida uma mensagem
+            if (andarNum < andarInicial || andarNum > andarFinal) {
+                console.log(`O andar ${andarNum} é inválido.`);
+                console.log(`Favor escolher um andar entre ${andarInicial} e ${andarFinal}.`);
+            } else {
+                // exibe o que o usuário digitou (output, saída)
+                console.log(`Andar escolhido: ${andarNum}`);
+            }
+
+            escolha();
+        }
+    });
+}
+
+// Função recursiva
+escolha();
